e9add49e95ca618cbcff451c68a18905e5310969
Sascha Grossenbacher <saschagros@gmail.com>
2012-04-24 09:18:09 +0200

    Converted to PSR-0

---
+++ core/modules/field/lib/Drupal/field/FieldUpdateForbiddenException.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * @file
+ * Definition of Drupal\field\FieldExeption.
+ */
+
+namespace Drupal\field;
+
+/**
+ * Exception class thrown by hook_field_update_forbid().
+ */
+class FieldUpdateForbiddenException extends FieldException {}
