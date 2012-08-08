a66f4b22af9d2b859bdea7a50b6da3e6e7b97a6a
Sascha Grossenbacher <saschagros@gmail.com>
2012-08-08 23:06:50 +0200

    removed unecessary controller, removed documentation.

--- core/modules/field/tests/modules/field_test/lib/Drupal/field_test/TestEntityController.php
+++ core/modules/field/tests/modules/field_test/lib/Drupal/field_test/TestEntityController.php
@@ -11,18 +11,26 @@ use Drupal\entity\DatabaseStorageController;
 use Drupal\entity\EntityInterface;
 
 /**
- * Controller class for the test_entity_bundle entity type.
+ * Controller class for the test entity entity types.
  */
 class TestEntityController extends DatabaseStorageController {
 
+  /**
+   * Overrides Drupal\entity\DatabaseStorageController::preSave().
+   */
   public function preSave(EntityInterface $entity) {
+    // Prepare for a new revision.
     if (!$entity->isNew() && !empty($entity->revision)) {
       $entity->old_ftvid = $entity->ftvid;
       $entity->ftvid = NULL;
     }
   }
 
+  /**
+   * Overrides Drupal\entity\DatabaseStorageController::postSave().
+   */
   public function postSave(EntityInterface $entity, $update) {
+    // Only the test_entity entity type has revisions.
     if ($entity->entityType() == 'test_entity') {
       $update_entity = TRUE;
       if (!$update || !empty($entity->revision)) {
@@ -32,7 +40,6 @@ class TestEntityController extends DatabaseStorageController {
         drupal_write_record('test_entity_revision', $entity, 'ftvid');
         $update_entity = FALSE;
       }
-
       if ($update_entity) {
         db_update('test_entity')
           ->fields(array('ftvid' => $entity->ftvid))
