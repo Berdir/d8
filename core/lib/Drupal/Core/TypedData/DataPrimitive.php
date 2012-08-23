e348dc82406b8eb14027c06c9a2fe91fa21cbfd9
Dick Olsson <dick.olsson@senzilla.com>
2012-08-23 14:07:36 +0200

    Revisted how primitives are defined and added a final DataPrimitive class

---
+++ core/lib/Drupal/Core/TypedData/DataPrimitive.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\Core\TypedData\DataPrimitives.
+ */
+
+/**
+ * Class that holds constants for all primitive data types.
+ */
+final class DataPrimitive {
+
+  /**
+   * The BOOLEAN primitive data type.
+   */
+  const BOOLEAN = 1;
+
+  /**
+   * The STRING primitive data type.
+   */
+  const STRING = 2;
+
+  /**
+   * The INTEGER primitive data type.
+   */
+  const INTEGER = 3;
+
+  /**
+   * The DECIMAL primitive data type.
+   */
+  const DECIMAL = 4;
+
+  /**
+   * The DATE primitive data type.
+   */
+  const DATE = 5;
+
+  /**
+   * The DURATION primitive data type.
+   */
+  const DURATION = 6;
+
+  /**
+   * The URI primitive data type.
+   */
+  const URI = 7;
+
+}
