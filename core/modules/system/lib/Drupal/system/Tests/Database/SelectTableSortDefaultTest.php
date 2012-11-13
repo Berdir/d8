9d2e70253ec3e58d0de2c35351162bb505bd9599
Sascha Grossenbacher <saschagros@gmail.com>
2012-11-13 09:01:35 +0100

    add a webtest base class, converted some to unit test, remaining to drupal unit test.

--- core/modules/system/lib/Drupal/system/Tests/Database/SelectTableSortDefaultTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Database/SelectTableSortDefaultTest.php
@@ -10,7 +10,7 @@ namespace Drupal\system\Tests\Database;
 /**
  * Tests the tablesort query extender.
  */
-class SelectTableSortDefaultTest extends DatabaseTestBase {
+class SelectTableSortDefaultTest extends DatabaseWebTestBase {
 
   public static function getInfo() {
     return array(
@@ -40,6 +40,7 @@ class SelectTableSortDefaultTest extends DatabaseTestBase {
       $this->drupalGet('database_test/tablesort/', array('query' => array('order' => $sort['field'], 'sort' => $sort['sort'])));
       $data = json_decode($this->drupalGetContent());
 
+      debug($data);
       $first = array_shift($data->tasks);
       $last = array_pop($data->tasks);
 
