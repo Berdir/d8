3232b8a7dcaddff777c0d03f42aea7c38e896c5b
Sascha Grossenbacher <saschagros@gmail.com>
2012-12-09 15:29:43 +0100

    coding style, implemented locale date upgrade.

--- core/modules/system/lib/Drupal/system/Tests/Upgrade/DateUpgradePathTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Upgrade/DateUpgradePathTest.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Definition of Drupal\system\Tests\Upgrade\DateUpgradePathTest.
+ * Contains Drupal\system\Tests\Upgrade\DateUpgradePathTest.
  */
 
 namespace Drupal\system\Tests\Upgrade;
@@ -33,7 +33,7 @@ class DateUpgradePathTest extends UpgradePathTestBase {
   public function testDateUpgrade() {
     $this->assertTrue($this->performUpgrade(), 'The upgrade was completed successfully.');
 
-    // Verify standard date formats
+    // Verify standard date formats.
     $expected_formats['short'] = array(
       'name' => 'Short',
       'pattern' => array(
@@ -56,7 +56,7 @@ class DateUpgradePathTest extends UpgradePathTestBase {
       'locked' => '1',
     );
 
-    // Verify custom date format
+    // Verify custom date format.
     $expected_formats['test_custom'] = array(
       'name' => 'Test Custom',
       'pattern' => array(
@@ -68,9 +68,52 @@ class DateUpgradePathTest extends UpgradePathTestBase {
     foreach ($expected_formats as $type => $format) {
       $format_info = config('system.date')->get('formats.' . $type);
 
-      $this->assertEqual($format_info['name'], $format['name'], "Config value for {$type} name is the same");
-      $this->assertEqual($format_info['locked'], $format['locked'], "Config value for {$type} locked is the same");
-      $this->assertEqual($format_info['pattern']['php'], $format['pattern']['php'], "Config value for {$type} PHP date pattern is the same");
+      $this->assertEqual($format_info['name'], $format['name'], format_string('Config value for @type name is the same', array('@type' => $type)));
+      $this->assertEqual($format_info['locked'], $format['locked'], format_string('Config value for @type locked is the same', array('@type' => $type)));
+      $this->assertEqual($format_info['pattern']['php'], $format['pattern']['php'], format_string('Config value for @type PHP date pattern is the same', array('@type' => $type)));
+
+      // Make sure that the variable was deleted.
+      $this->assertNull(update_variable_get('date_format_' . $type), format_string('Date format variable for @type was deleted.', array('@type' => $type)));
+    }
+
+    $expected_locale_formats = array(
+      array(
+        'langcode' => 'en',
+        'type' => 'long',
+        'format' => 'l, j F, Y - H:i',
+      ),
+      array(
+        'langcode' => 'en',
+        'type' => 'medium',
+        'format' => 'D, m/d/Y - H:i',
+      ),
+      array(
+        'langcode' => 'en',
+        'type' => 'short',
+        'format' => 'm/d/Y - H:i',
+      ),
+      array(
+        'langcode' => 'de',
+        'type' => 'long',
+        'format' => 'l, j. F, Y - H:i',
+      ),
+      array(
+        'langcode' => 'de',
+        'type' => 'medium',
+        'format' => 'D, d/m/Y - H:i',
+      ),
+      array(
+        'langcode' => 'de',
+        'type' => 'short',
+        'format' => 'd/m/Y - H:i',
+      ),
+    );
+
+    $config['en'] = config('locale.config.en.system.date');
+    $config['de'] = config('locale.config.de.system.date');
+    foreach ($expected_locale_formats as $locale_format) {
+      $format = $config[$locale_format['langcode']]->get('formats.' . $locale_format['type'] . '.pattern.php');
+      $this->assertEqual($locale_format['format'], $format);
     }
   }
 }
