eea17c2da5d87dbfdd721a8ac413e0b86bfc4bf0
Sascha Grossenbacher <saschagros@gmail.com>
2013-01-01 17:03:19 +0100

    convert update_free_access to settings

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -225,7 +225,7 @@ $databases = array();
  * After finishing the upgrade, be sure to open this file again and change the
  * TRUE back to a FALSE!
  */
-$update_free_access = FALSE;
+$settings['update_free_access'] = FALSE;
 
 /**
  * Salt for one-time login links and cancel links, form tokens, etc.
