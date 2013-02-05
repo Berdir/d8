5483f6b4f27a568b61b2feb686270a3833dc852a
Sascha Grossenbacher <saschagros@gmail.com>
2013-02-05 21:04:15 +0100

    applied patch

---
+++ core/modules/comment/lib/Drupal/comment/CommentAccessController.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\comment\CommentAccessController
+ */
+
+namespace Drupal\comment;
+
+use Drupal\Core\Entity\EntityAccessController;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\user\Plugin\Core\Entity\User;
+
+/**
+ * Access controller for the comment entity.
+ *
+ * @see \Drupal\comment\Plugin\Core\Entity\Comment.
+ */
+class CommentAccessController extends EntityAccessController {
+
+  /**
+   * Overrides \Drupal\Core\Entity\EntityAccessController::viewAccess().
+   */
+  public function viewAccess(EntityInterface $entity, $langcode = LANGUAGE_DEFAULT, User $account = NULL) {
+    return user_access('access comments', $account);
+  }
+
+  /**
+   * Overrides \Drupal\Core\Entity\EntityAccessController::createAccess().
+   */
+  public function createAccess(EntityInterface $entity, $langcode = LANGUAGE_DEFAULT, User $account = NULL) {
+    return user_access('post comments', $account);
+  }
+
+  /**
+   * Overrides \Drupal\Core\Entity\EntityAccessController::updateAccess().
+   */
+  public function updateAccess(EntityInterface $entity, $langcode = LANGUAGE_DEFAULT, User $account = NULL) {
+    // If no user is specified fill in the current one.
+    if (!isset($account)) {
+      $account = $GLOBALS['user'];
+    }
+    return ($account->uid && $account->id() == $entity->uid && $entity->status == COMMENT_PUBLISHED && user_access('edit own comments', $account)) || user_access('administer comments', $account);
+  }
+
+  /**
+   * Overrides \Drupal\Core\Entity\EntityAccessController::deleteAccess().
+   */
+  public function deleteAccess(EntityInterface $entity, $langcode = LANGUAGE_DEFAULT, User $account = NULL) {
+    return user_access('administer comments', $account);
+  }
+
+  /**
+   * Implements \Drupal\Core\Entity\EntityAccessController::approveAccess().
+   */
+  public function improveAccess(EntityInterface $entity, $langcode = LANGUAGE_DEFAULT, User $account = NULL) {
+    return user_access('administer comments', $account);
+  }
+
+}
