182d9a0d57f49ce989ec58d8baded079ba03b59a
Sascha Grossenbacher <saschagros@gmail.com>
2013-02-27 08:23:40 +0100

    added theme support to layout manager

--- core/modules/layout/lib/Drupal/layout/Plugin/Type/LayoutManager.php
+++ core/modules/layout/lib/Drupal/layout/Plugin/Type/LayoutManager.php
@@ -29,11 +29,38 @@ class LayoutManager extends PluginManagerBase {
    *   An array of paths keyed by it's corresponding namespaces.
    */
   public function __construct($namespaces) {
+    // Allow themes to provide plugins.
+    $namespaces += $this->getThemeNamespaces();
+
     // Create layout plugin derivatives from declaratively defined layouts.
     $this->discovery = new AnnotatedClassDiscovery('layout', 'layout', $namespaces);
     $this->discovery = new DerivativeDiscoveryDecorator($this->discovery);
     $this->discovery = new ProcessDecorator($this->discovery, array($this, 'processDefinition'));
 
     $this->factory = new ReflectionFactory($this->discovery);
+  }
+
+  /**
+   * Returns namespaces for themes to be used for the annotated class discovery.
+   *
+   * @todo: Move into the base class?
+   *
+   * @return array
+   *   Array of namespaces keyed by the directory.
+   */
+  protected function getThemeNamespaces() {
+    global $theme, $base_theme_info;
+    $namespaces = array();
+    if (isset($theme)) {
+      $theme_keys = array();
+      foreach ($base_theme_info as $base) {
+        $theme_keys[] = $base->name;
+      }
+      $theme_keys[] = $theme;
+      foreach ($theme_keys as $theme_key) {
+        $namespaces[drupal_get_path('theme', $theme_key) . '/lib'] = 'Drupal\\' . $theme_key;
+      }
+    }
+    return $namespaces;
   }
 }
