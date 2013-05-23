c7a656d371874d4860d5c491128e6aff5d23b3ed
Sascha Grossenbacher <saschagros@gmail.com>
2013-05-22 19:54:15 -0700

    applied patch

--- core/modules/system/lib/Drupal/system/Tests/Module/ModuleEnable.php
+++ core/modules/system/lib/Drupal/system/Tests/Module/ModuleEnable.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\system\Tests\Module;
 
+use Drupal\Core\Extension\ExtensionNameLengthException;
 use Drupal\simpletest\WebTestBase;
 
 /**
@@ -42,6 +43,21 @@ class ModuleEnable extends WebTestBase {
     $this->assertTrue($version > 0, 'System module version is > 0.');
     $version = drupal_get_installed_schema_version('user', TRUE);
     $this->assertTrue($version > 0, 'User module version is > 0.');
+  }
+
+  /**
+   * Tests that an exception is thrown when a module name is too long.
+   */
+  function testModuleNameLength() {
+    $module_name = 'invalid_module_name_over_the_maximum_allowed_character_length';
+    $message = format_string('Exception thrown when enabling module %name with a name length over the allowed maximum', array('%name' => $module_name));
+    try {
+      module_enable(array($module_name));
+      $this->fail($message);
+    }
+    catch (ExtensionNameLengthException $e) {
+      $this->pass($message);
+    }
   }
 
 }
