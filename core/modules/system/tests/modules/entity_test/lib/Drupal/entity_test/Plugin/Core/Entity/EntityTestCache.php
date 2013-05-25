1d41e4dbfac17a525d5f257b9da21b963f3e38df
Sascha Grossenbacher <saschagros@gmail.com>
2013-05-25 13:57:17 -0700

    take over stuff

--- core/modules/system/tests/modules/entity_test/lib/Drupal/entity_test/Plugin/Core/Entity/EntityTestCache.php
+++ core/modules/system/tests/modules/entity_test/lib/Drupal/entity_test/Plugin/Core/Entity/EntityTestCache.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Definition of Drupal\entity_test\Plugin\Core\Entity\EntityTest.
+ * Definition of Drupal\entity_test\Plugin\Core\Entity\EntityTestCache.
  */
 
 namespace Drupal\entity_test\Plugin\Core\Entity;
@@ -10,14 +10,13 @@ namespace Drupal\entity_test\Plugin\Core\Entity;
 use Drupal\Core\Entity\EntityNG;
 use Drupal\Core\Entity\Annotation\EntityType;
 use Drupal\Core\Annotation\Translation;
-use Drupal\Core\Language\Language;
 
 /**
  * Defines the test entity class.
  *
  * @EntityType(
- *   id = "entity_test",
- *   label = @Translation("Test entity"),
+ *   id = "entity_test_cache",
+ *   label = @Translation("Test entity with field cache"),
  *   module = "entity_test",
  *   controllers = {
  *     "storage" = "Drupal\entity_test\EntityTestStorageController",
@@ -29,16 +28,16 @@ use Drupal\Core\Language\Language;
  *   },
  *   base_table = "entity_test",
  *   fieldable = TRUE,
- *   field_cache = FALSE,
+ *   field_cache = TRUE,
  *   entity_keys = {
  *     "id" = "id",
  *     "uuid" = "uuid",
- *     "bundle" = "type"
+ *     "bundle" = "type",
  *   },
  *   menu_base_path = "entity-test/manage/%entity_test"
  * )
  */
-class EntityTest extends EntityNG {
+class EntityTestCache extends EntityNG {
 
   /**
    * The entity ID.
@@ -91,7 +90,7 @@ class EntityTest extends EntityNG {
   /**
    * Overrides Drupal\entity\Entity::label().
    */
-  public function label($langcode = Language::LANGCODE_DEFAULT) {
+  public function label($langcode = LANGUAGE_DEFAULT) {
     $info = $this->entityInfo();
     if (isset($info['entity_keys']['label']) && $info['entity_keys']['label'] == 'name') {
       return $this->getTranslation($langcode)->name->value;
