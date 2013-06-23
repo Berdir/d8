13d74f10d7fe719dd060207a1482215e0708a07c
Sascha Grossenbacher <saschagros@gmail.com>
2013-06-23 16:51:39 +0200

    applied patch

--- core/modules/node/lib/Drupal/node/NodeInterface.php
+++ core/modules/node/lib/Drupal/node/NodeInterface.php
@@ -14,4 +14,110 @@ use Drupal\Core\Entity\ContentEntityInterface;
  */
 interface NodeInterface extends ContentEntityInterface {
 
+  /**
+   * Sets the node title.
+   *
+   * @param string $title
+   *   The node title.
+   */
+  public function setTitle($title);
+
+  /**
+   * Returns the node creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the node.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the node creation timestamp.
+   *
+   * @param int $timestamp
+   *   The node creation timestamp.
+   */
+  public function setCreatedTime($timestamp);
+
+  /**
+   * Returns the node modification timestamp.
+   *
+   * @return int
+   *   Node creation timestamp.
+   */
+  public function getChangedTime();
+
+  /**
+   * Returns the node promotion status.
+   *
+   * @return bool
+   *   TRUE if the node is promoted.
+   */
+  public function isPromoted();
+
+  /**
+   * Sets the node promoted status.
+   *
+   * @param bool $promoted
+   *   TRUE to set this node to promoted, FALSE to set it to not promoted.
+   */
+  public function setPromoted($promoted);
+
+  /**
+   * Returns the node sticky status.
+   *
+   * @return bool
+   *   TRUE if the node is sticky.
+   */
+  public function isSticky();
+
+  /**
+   * Sets the node sticky status.
+   *
+   * @param bool $sticky
+   *   TRUE to set this node to sticky, FALSE to set it to not sticky.
+   */
+  public function setSticky($sticky);
+
+  /**
+   * Returns the node author user entity.
+   *
+   * @return \Drupal\user\UserInterface
+   *   The author user entity.
+   */
+  public function getAuthor();
+
+  /**
+   * Returns the node author user ID.
+   *
+   * @return int
+   *   The author user ID.
+   */
+  public function getAuthorId();
+
+  /**
+   * Sets the node author user ID.
+   *
+   * @param int $uid
+   *   The author user id.
+   */
+  public function setAuthorId($uid);
+
+  /**
+   * Returns the node published status indicator.
+   *
+   * Unpublished nodes are only visible to their authors and to administrators.
+   *
+   * @return bool
+   *   TRUE if the node is published.
+   */
+  public function isPublished();
+
+  /**
+   * Sets the published status of a node..
+   *
+   * @param bool $published
+   *   TRUE to set this node to published, FALSE to set it to unpublished.
+   */
+  public function setPublished($published);
+
 }
--- core/modules/node/lib/Drupal/node/Plugin/Core/Entity/Node.php
+++ core/modules/node/lib/Drupal/node/Plugin/Core/Entity/Node.php
@@ -60,175 +60,6 @@ use Drupal\node\NodeBCDecorator;
 class Node extends EntityNG implements NodeInterface {
 
   /**
-   * The node ID.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $nid;
-
-  /**
-   * The node revision ID.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $vid;
-
-  /**
-   * The node UUID.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $uuid;
-
-  /**
-   * The node content type (bundle).
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $type;
-
-  /**
-   * The node language code.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $langcode;
-
-  /**
-   * The node title.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $title;
-
-  /**
-   * The node owner's user ID.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $uid;
-
-  /**
-   * The node published status indicator.
-   *
-   * Unpublished nodes are only visible to their authors and to administrators.
-   * The value is either NODE_PUBLISHED or NODE_NOT_PUBLISHED.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $status;
-
-  /**
-   * The node creation timestamp.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $created;
-
-  /**
-   * The node modification timestamp.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $changed;
-
-  /**
-   * The node comment status indicator.
-   *
-   * COMMENT_NODE_HIDDEN => no comments
-   * COMMENT_NODE_CLOSED => comments are read-only
-   * COMMENT_NODE_OPEN => open (read/write)
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $comment;
-
-  /**
-   * The node promotion status.
-   *
-   * Promoted nodes should be displayed on the front page of the site. The value
-   * is either NODE_PROMOTED or NODE_NOT_PROMOTED.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $promote;
-
-  /**
-   * The node sticky status.
-   *
-   * Sticky nodes should be displayed at the top of lists in which they appear.
-   * The value is either NODE_STICKY or NODE_NOT_STICKY.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $sticky;
-
-  /**
-   * The node translation set ID.
-   *
-   * Translations sets are based on the ID of the node containing the source
-   * text for the translation set.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $tnid;
-
-  /**
-   * The node translation status.
-   *
-   * If the translation page needs to be updated, the value is 1; otherwise 0.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $translate;
-
-  /**
-   * The node revision creation timestamp.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $revision_timestamp;
-
-  /**
-   * The node revision author's user ID.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $revision_uid;
-
-  /**
-   * The node revision log message.
-   *
-   * @var \Drupal\Core\Entity\Field\FieldInterface
-   */
-  public $log;
-
-  /**
-   * Overrides \Drupal\Core\Entity\EntityNG::init().
-   */
-  protected function init() {
-    parent::init();
-    // We unset all defined properties, so magic getters apply.
-    unset($this->nid);
-    unset($this->vid);
-    unset($this->uuid);
-    unset($this->type);
-    unset($this->title);
-    unset($this->uid);
-    unset($this->status);
-    unset($this->created);
-    unset($this->changed);
-    unset($this->comment);
-    unset($this->promote);
-    unset($this->sticky);
-    unset($this->tnid);
-    unset($this->translate);
-    unset($this->revision_timestamp);
-    unset($this->revision_uid);
-    unset($this->log);
-  }
-
-  /**
    * Implements Drupal\Core\Entity\EntityInterface::id().
    */
   public function id() {
@@ -310,5 +141,95 @@ class Node extends EntityNG implements NodeInterface {
     }
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function setTitle($title) {
+    $this->set('title', $title);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChangedTime() {
+    return $this->get('changed')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPromoted() {
+    return (bool) $this->get('promote')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isSticky() {
+    return (bool) $this->get('sticky')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isPublished() {
+    return (bool) $this->get('status')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAuthor() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAuthorId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPromoted($promoted) {
+    $this->set('promoted', $promoted ? NODE_PROMOTED : NODE_NOT_PROMOTED);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setSticky($sticky) {
+    $this->set('sticky', $sticky ? NODE_STICKY : NODE_NOT_STICKY);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAuthorId($uid) {
+    $this->set('uid', $uid);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPublished($published) {
+    $this->set('status', $published ? NODE_PUBLISHED : NODE_NOT_PUBLISHED);
+  }
 
 }
