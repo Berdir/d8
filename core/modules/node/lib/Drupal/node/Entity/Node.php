2749141cfa0a9427e6247dd4ff210b4beb92e13a
Sascha Grossenbacher <saschagros@gmail.com>
2013-07-26 11:38:45 +0200

    ng fixes

--- core/modules/node/lib/Drupal/node/Entity/Node.php
+++ core/modules/node/lib/Drupal/node/Entity/Node.php
@@ -116,7 +116,7 @@ class Node extends EntityNG implements NodeInterface {
     // default revision. There's no need to delete existing records if the node
     // is new.
     if ($this->isDefaultRevision()) {
-      \Drupal::entityManager()->getAccessController('node')->writeGrants($this->getBCEntity(), $update);
+      \Drupal::entityManager()->getAccessController('node')->writeGrants($this, $update);
     }
   }
 
