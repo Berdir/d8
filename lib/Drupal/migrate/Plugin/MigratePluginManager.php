30644a4439305c03c8e175d53211f657d337062e
Karoly Negyesi <chx1975@gmail.com>
2013-10-05 16:12:13 -0700

    generalize the plugin manager

--- lib/Drupal/migrate/Plugin/MigratePluginManager.php
+++ lib/Drupal/migrate/Plugin/MigratePluginManager.php
@@ -17,11 +17,13 @@ use Drupal\Core\Plugin\DefaultPluginManager;
  *
  * @see hook_migrate_info_alter()
  */
-class MigrateSourcePluginManager extends DefaultPluginManager {
+class MigratePluginManager extends DefaultPluginManager {
 
   /**
    * Constructs a MigraterPluginManager object.
    *
+   * @param string $type
+   *   The type of the plugin: source, mapping, destination.
    * @param \Traversable $namespaces
    *   An object that implements \Traversable which contains the root paths
    *   keyed by the corresponding namespace to look for plugin implementations.
@@ -32,10 +34,10 @@ class MigrateSourcePluginManager extends DefaultPluginManager {
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
-    parent::__construct('Plugin/migrate/source', $namespaces, 'Drupal\Component\Annotation\PluginID');
-    $this->alterInfo($module_handler, 'migrate_source_info');
-    $this->setCacheBackend($cache_backend, $language_manager, 'migrate_plugins');
+  public function __construct($type, \Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
+    parent::__construct("Plugin/migrate/$type", $namespaces, 'Drupal\Component\Annotation\PluginID');
+    $this->alterInfo($module_handler, 'migrate_' . $type . '_info');
+    $this->setCacheBackend($cache_backend, $language_manager, 'migrate_plugins_' . $type);
   }
 
 }
