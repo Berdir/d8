1748019279ca95427c9510a774188bc135e42572
Karoly Negyesi <chx1975@gmail.com>
2013-10-22 20:41:51 -0700

    yay taxonomy source and tests thanks bdone

---
+++ lib/Drupal/migrate/Plugin/migrate/source/d6/Taxonomy.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\source\d6\Taxonomy.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\source\d6;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Drupal 6 taxonomy source.
+ *
+ * @PluginId("drupal6_taxonomy")
+ */
+class Taxonomy extends SqlBase {
+
+  /**
+   * The machine name, or vocabulary ID (pre-D7), of the vocabulary we're
+   * migrating from.
+   *
+   * @var mixed
+   */
+  protected $sourceVocabulary;
+
+  function query() {
+
+    // @todo: determine how to pass in arguments via plugin config constructor
+    $this->sourceVocabulary = '5,6';
+
+    // Note the explode - this supports the (admittedly unusual) case of
+    // consolidating multiple vocabularies into one.
+    $query = $this->database
+      ->select('term_data', 'td')
+      ->fields('td', array('tid', 'vid', 'name', 'description', 'weight'))
+      ->condition('vid', explode(',', $this->sourceVocabulary), 'IN')
+      // @todo: working, but not is there support for distinct() in FakeSelect?
+      ->distinct();
+    // Join to the hierarchy so we can sort on parent, but we'll pull the
+    // actual parent values in separately in case there are multiples.
+    $query->leftJoin('term_hierarchy', 'th', 'td.tid = th.tid');
+    $query->fields('th', array('parent'));
+    $query->orderBy('parent');
+    return $query;
+  }
+
+  /**
+   * Derived classes must implement fields(), returning a list of available
+   * source fields.
+   *
+   * @return array
+   *   Keys: machine names of the fields (to be passed to addFieldMapping)
+   *   Values: Human-friendly descriptions of the fields.
+   */
+  public function fields() {
+    // TODO: Implement fields() method.
+  }
+
+  /**
+   * Derived classes must implement computeCount(), to retrieve a fresh count of
+   * source records.
+   *
+   * @return int
+   */
+  function computeCount() {
+    // TODO: Implement computeCount() method.
+  }
+}
---
+++ tests/Drupal/migrate/Tests/D6TaxonomySourceTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Tests\D6TaxonomySourceTest.
+ */
+
+namespace Drupal\migrate\Tests;
+
+/**
+ * Tests taxonomy migration from D6 to D8.
+ *
+ * @group migrate
+ */
+class D6TaxonomySourceTest extends MigrateSqlSourceTestCase {
+
+  const PLUGIN_CLASS = 'Drupal\migrate\Plugin\migrate\source\d6\Taxonomy';
+
+  const PLUGIN_ID = 'drupal6_taxonomy';
+
+  const BASE_TABLE = 'term_data';
+
+  const BASE_ALIAS = 'td';
+
+  protected $migrationConfiguration = array(
+    'id' => 'test',
+    'highwaterProperty' => array('field' => 'test'),
+    'idlist' => array(),
+    'sourceKeys' => array(
+      'tid' => array(
+        'alias' => 't',
+      ),
+    ),
+    'destinationKeys' => array(
+      'tid' => array(),
+    ),
+  );
+
+  protected $results = array(
+    array(
+      'tid' => 1,
+      'vid' => 5,
+      'name' => 'name value 1',
+      'description' => 'description value 1',
+      'weight' => 0,
+      'parent' => 0,
+    ),
+    array(
+      'tid' => 2,
+      'vid' => 6,
+      'name' => 'name value 2',
+      'description' => 'description value 2',
+      'weight' => 1,
+      'parent' => 1,
+    ),
+  );
+
+  public static function getInfo() {
+    return array(
+      'name' => 'D6 taxonomy source functionality',
+      'description' => 'Tests D6 taxonomy source plugin.',
+      'group' => 'Migrate',
+    );
+  }
+
+  public function setUp() {
+    foreach ($this->results as $k => $row) {
+      $this->databaseContents['term_hierarchy'][$k]['tid'] = $row['tid'];
+      $this->databaseContents['term_hierarchy'][$k]['parent'] = $row['parent'];
+      unset($row['parent']);
+      $this->databaseContents['term_data'][$k] = $row;
+    }
+    parent::setUp();
+  }
+
+}
