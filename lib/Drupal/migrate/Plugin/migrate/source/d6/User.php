159ed2c9595fa11e734cd1d1ee796964d8a922ff
Marco Villegas <git@marvil07.net>
2013-10-24 23:21:50 -0500

    initial attempt on d6 user source

---
+++ lib/Drupal/migrate/Plugin/migrate/source/d6/User.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\source\d6\User.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\source\d6;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Drupal 6 user source from database.
+ *
+ * @PluginId("drupal6_user")
+ */
+class User extends SqlBase {
+  function query() {
+    $query = $this->database
+      ->select('users', 'u')
+      ->fields('u', array('uid', 'name', 'pass', 'mail', 'mode', 'sort', 'threshold', 'theme', 'signature', 'signature_format', 'created', 'access', 'login', 'status', 'timezone', 'language', 'picture', 'init', 'data', 'timezone_name'))
+      ->condition('uid', 0, '>');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return array(
+      'uid' => t('User ID'),
+      'name' => t('Username'),
+      'pass' => t('Password'),
+      'mail' => t('Email address'),
+      'mode' => t('Per-user comment display mode'),
+      'sort' => t('Per-user comment sort order'),
+      'threshold' => t('Obsolete comment configuration'),
+      'theme' => t('Default theme'),
+      'signature' => t('Signature'),
+      'signature_format' => t('Signature format'),
+      'created' => t('Registered timestamp'),
+      'access' => t('Last access timestamp'),
+      'login' => t('Last login timestamp'),
+      'status' => t('Status'),
+      'timezone' => t('Timezone'),
+      'language' => t('Language'),
+      'picture' => t('Picture'),
+      'init' => t('Init'),
+      'data' => t('Data'),
+    );
+  }
+}
---
+++ tests/Drupal/migrate/Tests/D6UserSourceTest.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Tests\D6CommentSourceTest.
+ */
+
+namespace Drupal\migrate\Tests;
+
+/**
+ * Tests comment migration from D6 to D8.
+ *
+ * @group migrate
+ */
+class D6UserSourceTest extends MigrateSqlSourceTestCase {
+
+  const PLUGIN_CLASS = 'Drupal\migrate\Plugin\migrate\source\d6\User';
+
+  const PLUGIN_ID = 'drupal6_user';
+
+  const BASE_TABLE = 'users';
+
+  const BASE_ALIAS = 'u';
+
+  protected $migrationConfiguration = array(
+    'id' => 'test',
+    'idlist' => array(),
+    'sourceKeys' => array(
+      'uid' => array(
+        // This is where the field schema would go.
+        'alias' => 'u',
+      ),
+    ),
+    'destinationKeys' => array(
+      'uid' => array(
+        // This is where the field schema would go.
+      ),
+    ),
+  );
+
+  protected $results = array(
+    array(
+      'uid' => '1',
+      'name' => 'admin',
+      // @todo d6 hash?
+      'pass' => '1234',
+      'mail' => 'admin@example.com',
+      'mode' => 0,
+      'sort' => 0,
+      'threshold' => 0,
+      'theme' => '',
+      'signature' => '',
+      'signature_format' => 0,
+      'created' => 1279402616,
+      'access' => 1322981278,
+      'login' => 1322699994,
+      'status' => 0,
+      'timezone' => 'America/Lima',
+      'language' => 'en',
+      // @todo Add the file when needed.
+      'picture' => 'sites/default/files/pictures/picture-1.jpg',
+      'init' => 'admin@example.com',
+      'data' => null,
+    ),
+    array(
+      'uid' => '4',
+      'name' => 'alice',
+      // @todo d6 hash?
+      'pass' => '1234',
+      'mail' => 'alice@example.com',
+      'mode' => 0,
+      'sort' => 0,
+      'threshold' => 0,
+      'theme' => '',
+      'signature' => '',
+      'signature_format' => 0,
+      'created' => 1322981368,
+      'access' => 1322982419,
+      'login' => 132298140,
+      'status' => 0,
+      'timezone' => 'America/Lima',
+      'language' => 'en',
+      'picture' => '',
+      'init' => 'alice@example.com',
+      'data' => null,
+    ),
+  );
+
+  public static function getInfo() {
+    return array(
+      'name' => 'D6 user source functionality',
+      'description' => 'Tests D6 user source plugin.',
+      'group' => 'Migrate',
+    );
+  }
+
+  public function setUp() {
+    foreach ($this->results as $k => $row) {
+      $this->databaseContents['users'][$k] = $row;
+    }
+    parent::setUp();
+  }
+
+}
