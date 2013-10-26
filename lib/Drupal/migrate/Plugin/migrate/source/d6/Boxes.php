f79b2b972ad19be2096f6264265abc6e8fb68b82
mpgeek <mpgeek@885874.no-reply.drupal.org>
2013-10-26 14:54:22 -0700

    D6 boxes source plugin + tests

---
+++ lib/Drupal/migrate/Plugin/migrate/source/d6/Boxes.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\source\d6\Boxes.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\source\d6;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Drupal 6 block source from database.
+ *
+ * @PluginId("drupal6_boxes")
+ */
+class Boxes extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  function query() {
+    $query = $this->database
+      ->select('bid', 'b')
+      ->fields('b', array('bid', 'body', 'info', 'format'));
+    $query->orderBy('bid');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return array(
+      'bid' => t('The numeric identifier of the block/box'),
+      'body' => t('The block/box content'),
+      'info' => t('Admin title of the block/box.'),
+      'format' => t('Input format of the custom block/box content.'),
+    );
+  }
+
+}
---
+++ tests/Drupal/migrate/Tests/D6BoxesSourceTest.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Tests\D6BoxesSourceTest.
+ */
+
+namespace Drupal\migrate\Tests;
+
+/**
+ * Tests Boxes migration from D6 to D8.
+ *
+ * @group migrate
+ */
+class D6BoxesSourceTest extends MigrateSqlSourceTestCase {
+
+  // The plugin system is not working during unit testing so the source plugin
+  // class needs to be manually specified.
+  const PLUGIN_CLASS = 'Drupal\migrate\Plugin\migrate\source\d6\Boxes';
+
+  // The table passed to $this->database->select.
+  const BASE_TABLE = 'boxes';
+
+  // The base alias passed to $this->database->select.
+  const BASE_ALIAS = 'b';
+
+  // The fake Migration configuration entity.
+  protected $migrationConfiguration = array(
+    // The id of the entity, can be any string.
+    'id' => 'test',
+    // Leave it empty for now.
+    'idlist' => array(),
+    'source' => array(
+      'plugin' => 'drupal6_boxes',
+    ),
+    // This needs to be the identifier of the actual key: cid for comment, nid
+    // for node and so on.
+    'sourceIds' => array(
+      // Box (block) ID.
+      'bid' => array(
+        // This is where the field schema would go but for now we need to
+        // specify the table alias for the key. Most likely this will be the
+        // same as BASE_ALIAS.
+        'alias' => 'b',
+      ),
+    ),
+    'destinationIds' => array(
+      'entity_id' => array(
+        // This is where the field schema would go.
+      ),
+      'deleted' => array(),
+      'delta' => array(),
+      'langcode' => array(),
+    ),
+  );
+
+  // We need to set up the database contents; it's easier to do that below.
+  // These are sample result queries.
+  protected $results = array(
+    array(
+      'bid' => 1,
+      'body' => '<p>I made some custom content.</p>',
+      'info' => 'Static Block',
+      'format' => 1,
+    ),
+    array(
+      'bid' => 2,
+      'body' => '<p>I made some more custom content.</p>',
+      'info' => 'Test Content',
+      'format' => 1,
+    ),
+  );
+
+  /**
+   * Prepopulate contents with results.
+   */
+  public function setUp() {
+    $this->databaseContents['boxes'] = $this->results;
+    parent::setUp();
+  }
+
+  /**
+   * Provide meta information about this battery of tests.
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'D6 block boxes source functionality',
+      'description' => 'Tests D6 block boxes source plugin.',
+      'group' => 'Migrate',
+    );
+  }
+
+}
