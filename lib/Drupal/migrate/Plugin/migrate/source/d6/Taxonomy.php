32311c52d6105a7dd1b29fe2aea1ba33885d7bd4
Marco Villegas <git@marvil07.net>
2013-10-26 20:05:22 -0500

    Pass vocabulary in d6 term source constructor.

--- lib/Drupal/migrate/Plugin/migrate/source/d6/Taxonomy.php
+++ lib/Drupal/migrate/Plugin/migrate/source/d6/Taxonomy.php
@@ -7,6 +7,9 @@
 
 namespace Drupal\migrate\Plugin\migrate\source\d6;
 
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\migrate\Entity\MigrationInterface;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
 
 /**
@@ -17,24 +20,37 @@ use Drupal\migrate\Plugin\migrate\source\SqlBase;
 class Taxonomy extends SqlBase {
 
   /**
-   * The machine name, or vocabulary ID (pre-D7), of the vocabulary we're
-   * migrating from.
+   * The vocabulary ID.
+   *
+   * As mentioned in query(), this can be a comma separated list of vocabulary
+   * ids.
    *
    * @var mixed
    */
-  protected $sourceVocabulary;
+  protected $vocabulary;
 
+  /**
+   * {@inheritdoc}
+   */
+  function __construct(array $configuration, $plugin_id, array $plugin_definition, MigrationInterface $migration, CacheBackendInterface $cache, KeyValueStoreInterface $highwater_storage) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $cache, $highwater_storage);
+    if (empty($configuration['vocabulary'])) {
+      // @todo MigrateException?
+      throw new \Exception('At least one vocabulary ID is required to instanciate a D6 Term source.');
+    }
+    $this->vocabulary = $configuration['vocabulary'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   function query() {
-
-    // @todo: determine how to pass in arguments via plugin config constructor
-    $this->sourceVocabulary = $this->configuration['vocabulary'];
-
     // Note the explode - this supports the (admittedly unusual) case of
     // consolidating multiple vocabularies into one.
     $query = $this->database
       ->select('term_data', 'td')
       ->fields('td', array('tid', 'vid', 'name', 'description', 'weight'))
-      ->condition('vid', explode(',', $this->sourceVocabulary), 'IN')
+      ->condition('vid', explode(',', $this->vocabulary), 'IN')
       // @todo: working, but not is there support for distinct() in FakeSelect?
       ->distinct();
     // Join to the hierarchy so we can sort on parent, but we'll pull the
