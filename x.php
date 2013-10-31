880051582e85369be4f19790f256ef81035414f6
Karoly Negyesi <chx1975@gmail.com>
2013-10-31 14:08:05 -0700

    and the first migration is here as well

---
+++ x.php
@@ -0,0 +1,25 @@
+<?php
+
+use Drupal\migrate\DrupalMessage;
+
+$values = array(
+  'id' => 'first',
+  'source' => array(
+    'plugin' => 'drupal6_variable',
+    'variables' => array(
+      'site_mail',
+      'site_name',
+    ),
+  ),
+  'process' => array(
+
+  ),
+  'destination' => array(
+    'plugin' => 'd8_config',
+    'config_name' => 'test',
+  ),
+);
+
+$migration = entity_create('migration', $values);
+$executable = new \Drupal\migrate\MigrateExecutable($migration, new DrupalMessage);
+$executable->import();
