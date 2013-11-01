65c4c69389aed36b219a57fb44e8ffc3cc5ab534
Brent Dunn <brentd@knowclassic.com>
2013-11-01 12:12:21 -0400

    FakeStatement: inheritdoc docblocks

--- tests/Drupal/migrate/Tests/FakeStatement.php
+++ tests/Drupal/migrate/Tests/FakeStatement.php
@@ -9,20 +9,35 @@ namespace Drupal\migrate\Tests;
 
 use Drupal\Core\Database\StatementInterface;
 
+/**
+ * Represents a fake prepared statement.
+ */
 class FakeStatement extends \ArrayIterator implements StatementInterface {
 
+  /**
+   * {@inheritdoc}
+   */
   public function execute($args = array(), $options = array()) {
     throw new \Exception('This method is not supported');
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getQueryString() {
     throw new \Exception('This method is not supported');
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function rowCount() {
     return $this->count();
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchField($index = 0) {
     $row = array_values($this->current());
     $return = $row[$index];
@@ -30,12 +45,18 @@ class FakeStatement extends \ArrayIterator implements StatementInterface {
     return $return;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchAssoc() {
     $return = $this->current();
     $this->next();
     return $return;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchCol($index = 0) {
     $return = array();
     foreach ($this as $row) {
@@ -45,6 +66,9 @@ class FakeStatement extends \ArrayIterator implements StatementInterface {
     return $return;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchAllKeyed($key_index = 0, $value_index = 1) {
     $return = array();
     foreach ($this as $row) {
@@ -54,6 +78,9 @@ class FakeStatement extends \ArrayIterator implements StatementInterface {
     return $return;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fetchAllAssoc($key, $fetch = NULL) {
     $return = array();
     foreach ($this as $row) {
