7b632c0037aa95a319fc394cb319f91d3b6a8b3c
bdone <brentd@knowclassic.com>
2013-11-01 15:48:01 -0400

    Issue #2125083 by bdone: doxygen for Row

--- lib/Drupal/migrate/Row.php
+++ lib/Drupal/migrate/Row.php
@@ -52,6 +52,9 @@ class Row {
    *   names.
    * @param array $values
    *   An array of values to add as properties on the object.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown when a source id property does not exist.
    */
   public function __construct(array $source_ids, array $values) {
     $this->source = $values;
@@ -65,14 +68,38 @@ class Row {
     }
   }
 
+  /**
+   * Retrieves the values of the source identifiers.
+   *
+   * @return array
+   *   An array containing the ids of the source.
+   */
   public function getSourceIdValues() {
     return $this->sourceIdValues;
   }
 
+  /**
+   * Determines whether a source has a property.
+   *
+   * @param string $property
+   *   A property on the source.
+   *
+   * @return bool
+   *   TRUE if the source has property; FALSE otherwise.
+   */
   public function hasSourceProperty($property) {
     return isset($this->source[$property]) || array_key_exists($property, $this->source);
   }
 
+  /**
+   * Retrieves a source property.
+   *
+   * @param string $property
+   *   A property on the source.
+   *
+   * @return mixed|null
+   *   The found returned property or NULL if not found.
+   */
   public function getSourceProperty($property) {
     if (isset($this->source[$property])) {
       return $this->source[$property];
@@ -83,16 +110,20 @@ class Row {
    * This returns the whole source array.
    *
    * @return array
+   *   An array of source plugins.
    */
   public function getSource() {
     return $this->source;
   }
 
   /**
-   * Sets a source property. This can only be called from the source plugin
+   * Sets a source property. This can only be called from the source plugin.
    *
    * @param string $property
+   *   A property on the source.
    * @param mixed $data
+   *   The property value to set on the source.
+   *
    * @throws \Exception
    */
   public function setSourceProperty($property, $data) {
@@ -111,28 +142,68 @@ class Row {
     $this->frozen = TRUE;
   }
 
+  /**
+   * Determines whether a destination has a property.
+   *
+   * @param string $property
+   *   A property on the destination.
+   *
+   * @return bool
+   *   TRUE if the destination has property; FALSE otherwise.
+   */
   public function hasDestinationProperty($property) {
     return isset($this->destination[$property]) || array_key_exists($property, $this->destination);
   }
 
+  /**
+   * Sets a destination property.
+   *
+   * @param string $property
+   *   A property on the destination.
+   * @param mixed $value
+   *   The property value to set on the destination.
+   */
   public function setDestinationProperty($property, $value) {
     $this->destination[$property] = $value;
   }
 
+  /**
+   * Sets destination properties.
+   *
+   * @param array $property_keys
+   *   An array of properties on the destination.
+   * @param mixed $value
+   *   The property value to set on the destination.
+   */
   public function setDestinationPropertyDeep(array $property_keys, $value) {
     NestedArray::setValue($this->destination, $property_keys, $value, TRUE);
   }
 
+  /**
+   * This returns the whole destination array.
+   *
+   * @return array
+   *   An array of destination plugins.
+   */
   public function getDestination() {
     return $this->destination;
   }
 
+  /**
+   * Sets the Migrate id mappings.
+   *
+   * @param array $id_map
+   *   An array of mappings between source ID and destination ID.
+   */
   public function setIdMap(array $id_map) {
     $this->idMap = $id_map;
   }
 
   /**
+   * Retrieves the Migrate id mappings.
+   *
    * @return array
+   *   An array of id mappings between source ID and destination ID.
    */
   public function getIdMap() {
     return $this->idMap;
@@ -158,13 +229,21 @@ class Row {
   }
 
   /**
+   * Returns if this id mapping needs an update.
+   *
    * @return bool
-   *   TRUE if the row needs updating.
+   *   TRUE if the row needs updating, FALSE otherwise.
    */
   public function needsUpdate() {
     return $this->idMap['needs_update'] == MigrateIdMapInterface::STATUS_NEEDS_UPDATE;
   }
 
+  /**
+   * Returns the hash for this id mapping.
+   *
+   * @return mixed
+   *   The hashed source id mapping.
+   */
   public function getHash() {
     return $this->idMap['hash'];
   }
