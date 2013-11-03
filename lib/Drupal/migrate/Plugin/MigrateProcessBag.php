d827e35c61a2d49f7d53d7646e7da21338d0d26d
Daniel Wehner <daniel.wehner@erdfisch.de>
2013-11-03 20:35:22 +0000

    document the migration process bag

--- lib/Drupal/migrate/Plugin/MigrateProcessBag.php
+++ lib/Drupal/migrate/Plugin/MigrateProcessBag.php
@@ -12,9 +12,14 @@ use Drupal\Component\Plugin\Exception\UnknownPluginException;
 use Drupal\Component\Plugin\PluginManagerInterface;
 use Drupal\migrate\Entity\MigrationInterface;
 
+/**
+ * Contains all active process plugins on a migration.
+ */
 class MigrateProcessBag extends DefaultPluginBag {
 
   /**
+   * The migration entity this process bag is attached to.
+   *
    * @var \Drupal\migrate\Entity\MigrationInterface
    */
   protected $migration;
@@ -22,9 +27,15 @@ class MigrateProcessBag extends DefaultPluginBag {
   protected $pluginKey = 'plugin';
 
   /**
-   * @param PluginManagerInterface $manager
+   * Constructs a new MigrateProcessBag.
+   *
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $manager
+   *   The plugin manager to initialize process plugins.
    * @param array $configurations
-   * @param MigrationInterface $migration
+   *   (optional) An associative array containing the initial configuration for
+   *   each plugin in the bag, keyed by plugin instance ID.
+   * @param \Drupal\migrate\Entity\MigrationInterface $migration
+   *   The migration entity this process bag is attached to.
    */
   public function __construct(PluginManagerInterface $manager, array $configurations = array(), MigrationInterface $migration) {
     parent::__construct($manager, $configurations);
@@ -34,14 +45,7 @@ class MigrateProcessBag extends DefaultPluginBag {
   /**
    * {@inheritdoc}
    *
-   * @return \Drupal\migrate\Plugin\ProcessInterface
-   */
-  public function &get($instance_id) {
-    return parent::get($instance_id);
-  }
-
-  /**
-   * {@inheritdoc}
+   * Extends in order to pass the migration entity to the plugin manager.
    */
   public function initializePlugin($instance_id) {
     $this->configurations[$instance_id] += array('plugin' => 'property_map');
