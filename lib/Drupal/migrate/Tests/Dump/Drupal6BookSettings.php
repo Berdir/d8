748bcbfa1478751dd1f1c178b6306416209a3e11
bdone <bdone@687670.no-reply.drupal.org>
2013-11-05 18:20:54 -0800

    Book variables to config

---
+++ lib/Drupal/migrate/Tests/Dump/Drupal6BookSettings.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Tests\Drupal6BookSettings.
+ */
+
+namespace Drupal\migrate\Tests\Dump;
+
+use Drupal\Core\Database\Connection;
+
+/**
+ * Database dump for testing book.settings.yml migration.
+ */
+class Drupal6BookSettings {
+
+  /**
+   * Mock the database schema and values.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The mocked database connection.
+   */
+  public static function load(Connection $database) {
+    $database->schema()->createTable('variable', array(
+      'fields' => array(
+        'name' => array(
+          'type' => 'varchar',
+          'length' => 128,
+          'not null' => TRUE,
+          'default' => '',
+        ),
+        'value' => array(
+          'type' => 'blob',
+          'not null' => TRUE,
+          'size' => 'big',
+          'translatable' => TRUE,
+        ),
+      ),
+      'primary key' => array(
+        'name',
+      ),
+      'module' => 'book',
+      'name' => 'variable',
+    ));
+    $database->insert('variable')->fields(array(
+      'name',
+      'value',
+    ))
+    ->values(array(
+      'name' => 'book_allowed_types',
+      'value' => 'a:1:{i:0;s:4:"book";}',
+    ))
+    ->values(array(
+      'name' => 'book_block_mode',
+      'value' => 's:9:"all pages";',
+    ))
+    ->values(array(
+      'name' => 'book_child_type',
+      'value' => 's:4:"book";',
+    ))
+    ->execute();
+  }
+}
