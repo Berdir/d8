ce48293685f20126eb074dd7197438fa8ef72927
jacintocapote <jacintocapote@348228.no-reply.drupal.org>
2013-11-09 21:14:13 -0800

    Add a source plugin for D6 field instances.

---
+++ lib/Drupal/migrate/Plugin/migrate/source/d6/FieldInstance.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\source\d6\FieldInstance.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\source\d6;
+
+use Drupal\migrate\Plugin\RequirementsInterface;
+use Drupal\migrate\Plugin\migrate\source\d6\Drupal6SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Drupal 6 field instances source from database.
+ *
+ * @PluginId("drupal6_fieldinstance")
+ */
+class FieldInstance extends Drupal6SqlBase implements RequirementsInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  function query() {
+    $query = $this->database
+      ->select('content_node_field_instance', 'cnfi')
+      ->fields('cnfi', array(
+        'field_name',
+        'type_name',
+        'weight',
+        'label',
+        'widget_type',
+        'widget_settings',
+        'display_settings',
+        'description',
+        'widget_module',
+        'widget_active',
+      ));
+
+    $query->orderBy('type_name');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return array(
+      'field_name' => t('The machine name of field.'),
+      'type_name' => t('Content type where is used this field.'),
+      'weight' => t('Weight.'),
+      'label' => t('A name to show.'),
+      'widget_type' => t('Widget type.'),
+      'widget_settings' => t('Serialize data with widget settings.'),
+      'display_settings' => t('Serialize data with display settings.'),
+      'description' => t('A description of field.'),
+      'widget_module' => t('Module that implements widget.'),
+      'widget_active' => t('Status of widget'),
+    );
+  }
+
+  function prepareRow(Row $row, $keep = TRUE) {
+    //Unserialize data
+    $widget_settings = unserialize($row->getSourceProperty('widget_settings'));
+    $display_settings = unserialize($row->getSourceProperty('display_settings'));
+    $row->setSourceProperty('widget_settings', $widget_settings);
+    $row->setSourceProperty('display_settings', $display_settings);
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkRequirements() {
+    return $this->moduleExists('content') && $this->getModuleSchemaVersion('content') >= 6001;
+  }
+
+}
---
+++ tests/Drupal/migrate/Tests/D6FieldInstanceSourceTest.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Tests\D6FieldInstanceSourceTest.
+ */
+
+namespace Drupal\migrate\Tests;
+
+/**
+ * Tests Field Instances migration from D6 to D8.
+ *
+ * @group migrate
+ */
+class D6FieldInstanceSourceTest extends MigrateSqlSourceTestCase {
+
+  // The plugin system is not working during unit testing so the source plugin
+  // class needs to be manually specified.
+  const PLUGIN_CLASS = 'Drupal\migrate\Plugin\migrate\source\d6\FieldInstance';
+
+  // The fake Migration configuration entity.
+  protected $migrationConfiguration = array(
+    // The id of the entity, can be any string.
+    'id' => 'test_fieldinstance',
+    // Leave it empty for now.
+    'idlist' => array(),
+    'source' => array(
+      'plugin' => 'drupal6_fieldinstance',
+    ),
+    // This needs to be the identifier of the actual key: cid for comment, nid
+    // for node and so on.
+    'sourceIds' => array(
+      'type_name' => array(
+        // This is where the field schema would go but for now we need to
+        // specify the table alias for the key. Most likely this will be the
+        // same as BASE_ALIAS.
+        'alias' => 'fi',
+      ),
+    ),
+    'destinationIds' => array(
+      'content_node_field_instance' => array(
+        // This is where the field schema would go.
+      ),
+    ),
+  );
+
+  // We need to set up the database contents; it's easier to do that below.
+  // These are sample result queries.
+  protected $results = array(
+    array(
+      'field_name' => 'field_body',
+      'type_name' => 'page',
+      'weight' => 1,
+      'label' => 'body',
+      'widget_type' => 'text_textarea',
+      'widget_settings' => '',
+      'display_settings' => '',
+      'description' => '',
+      'widget_module' => 'text',
+      'widget_active' => 1,
+    ),
+  );
+
+  /**
+   * Prepopulate contents with results.
+   */
+  public function setUp() {
+    $this->results[0]['widget_settings'] = array(
+      'rows' => 5,
+      'size' => 60,
+      'default_value' => array(
+        array(
+          'value' => '',
+          '_error_element' => 'default_value_widget][field_body][0][value',
+          'default_value_php' => '',
+        ),
+      ),
+    );
+    $this->results[0]['display_settings'] = array(
+      'label' => array(
+        'format' => 'above',
+        'exclude' => 0,
+      ),
+      'teaser' => array(
+        'format' => 'default',
+        'exclude' => 0,
+      ),
+      'full' => array(
+        'format' => 'default',
+        'exclude' => 0,
+      ),
+    );
+    $this->databaseContents['content_node_field_instance'] = $this->results;
+    $this->databaseContents['content_node_field_instance'][0]['widget_settings'] = serialize($this->databaseContents['content_node_field_instance'][0]['widget_settings']);
+    $this->databaseContents['content_node_field_instance'][0]['display_settings'] = serialize($this->databaseContents['content_node_field_instance'][0]['display_settings']);
+    parent::setUp();
+  }
+
+  /**
+   * Provide meta information about this battery of tests.
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'D6 field instance source functionality',
+      'description' => 'Tests D6 field instance source plugin.',
+      'group' => 'Migrate',
+    );
+  }
+
+}
+namespace Drupal\migrate\Tests\source\d6;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\migrate\Plugin\migrate\source\d6\FieldInstance;
+
+class TestFieldInstance extends FieldInstance {
+  function setDatabase(Connection $database) {
+    $this->database = $database;
+  }
+  function setModuleHandler(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
+}
