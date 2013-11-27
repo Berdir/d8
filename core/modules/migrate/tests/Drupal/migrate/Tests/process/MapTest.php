082dd31d7cb48e95df6a66fe4e2cb9b1cf1a5053
Karoly Negyesi <chx1975@gmail.com>
2013-11-26 22:48:19 -0800

    simplify map test quite a bit.

--- core/modules/migrate/tests/Drupal/migrate/Tests/process/MapTest.php
+++ core/modules/migrate/tests/Drupal/migrate/Tests/process/MapTest.php
@@ -6,6 +6,7 @@
 
 namespace Drupal\migrate\Tests\process;
 
+use Drupal\migrate\Plugin\migrate\process\Map;
 use Drupal\migrate\Plugin\migrate\process\TestMap;
 
 /**
@@ -24,32 +25,16 @@ class MapTest extends MigrateProcessTestCase {
     );
   }
 
-  function setUp() {
-    $this->row = $this->getMockBuilder('Drupal\migrate\Row')
-      ->disableOriginalConstructor()
-      ->getMock();
-    $this->migrateExecutable = $this->getMockBuilder('Drupal\migrate\MigrateExecutable')
-      ->disableOriginalConstructor()
-      ->getMock();
-    $this->plugin = new TestMap();
-    parent::setUp();
-  }
-
   function testMap() {
-    $map['foo']['bar'] = 'baz';
-    $this->plugin->setMap($map);
-    $value = $this->plugin->transform(array('foo', 'bar'), $this->migrateExecutable, $this->row, 'destinationproperty');
+    $row = $this->getMockBuilder('Drupal\migrate\Row')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $migrate_executable = $this->getMockBuilder('Drupal\migrate\MigrateExecutable')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $configuration['map']['foo']['bar'] = 'baz';
+    $plugin = new Map($configuration, 'map', array());
+    $value = $plugin->transform(array('foo', 'bar'), $migrate_executable, $row, 'destinationproperty');
     $this->assertSame($value, 'baz');
-  }
-}
-
-
-namespace Drupal\migrate\Plugin\migrate\process;
-
-class TestMap extends Map {
-  function __construct() {
-  }
-  function setMap($map) {
-    $this->configuration['map'] = $map;
   }
 }
