c2930ef3ac7bf580bbab3c0eb17ff343184b4506
Karoly Negyesi <chx1975@gmail.com>
2013-12-08 21:36:54 -0800

    add new Extract plugin (will need tests)

---
+++ core/modules/migrate/lib/Drupal/migrate/Plugin/migrate/process/Extract.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\process\MultipleColumnsMap.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\process;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Plugin\PluginBase;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\MigrateExecutable;
+use Drupal\migrate\Plugin\MigrateProcessInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin changes the current value based on a static lookup map.
+ *
+ * @see https://drupal.org/node/2143521
+ *
+ * @PluginId("extract")
+ */
+class Extract extends PluginBase implements MigrateProcessInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutable $migrate_executable, Row $row, $destination_property) {
+    if (!is_array($value)) {
+      throw new MigrateException('Extraction failed.');
+    }
+    $new_value = NestedArray::getValue($value, $this->configuration['indexes'], $key_exists);
+    if (!$key_exists) {
+      throw new MigrateException('Extraction failed.');
+    }
+    return $new_value;
+  }
+
+}
+
