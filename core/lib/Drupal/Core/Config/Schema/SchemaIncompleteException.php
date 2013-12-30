fd6e96ea2856cdbae5690a74d3226efae53aa49e
Sascha Grossenbacher <saschagros@gmail.com>
2013-12-30 21:58:31 +0100

    applied patch

---
+++ core/lib/Drupal/Core/Config/Schema/SchemaIncompleteException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\Core\Config\Schema\SchemaIncompleteException.
+ */
+
+namespace Drupal\Core\Config\Schema;
+
+/**
+ * An exception thrown when a config schema is incomplete.
+ */
+class SchemaIncompleteException extends \RuntimeException {}
