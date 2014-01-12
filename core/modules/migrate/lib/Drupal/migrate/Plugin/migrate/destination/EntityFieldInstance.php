cdfb34a91c8b9f3dbc9a8fc4f4ef7482de269856
Karoly Negyesi <chx1975@gmail.com>
2014-01-12 11:28:53 -0800

    Field instance destination filling in id automatically.

---
+++ core/modules/migrate/lib/Drupal/migrate/Plugin/migrate/destination/EntityFieldInstance.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\destination\EntityFieldInstance.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\destination;
+
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @PluginId("entity_field_instance")
+ */
+class EntityFieldInstance extends Entity {
+
+  public function import(Row $row) {
+    $row->setDestinationProperty('id', implode('.', array(
+      $row->getDestinationProperty('entity_type'),
+      $row->getDestinationProperty('bundle'),
+      $row->getDestinationProperty('field_name'),
+    )));
+    return parent::import($row);
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+    $configuration['entity_type'] = 'field_instance';
+    return parent::create($container, $configuration, $plugin_id, $plugin_definition);
+  }
+}
