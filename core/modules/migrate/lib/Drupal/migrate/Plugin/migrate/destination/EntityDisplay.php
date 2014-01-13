964c09acf0f51bea8459a1524576a65137bce697
Karoly Negyesi <chx1975@gmail.com>
2014-01-12 16:08:56 -0800

    entity display destination

---
+++ core/modules/migrate/lib/Drupal/migrate/Plugin/migrate/destination/EntityDisplay.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate\Plugin\migrate\destination\EntityDisplay.
+ */
+
+namespace Drupal\migrate\Plugin\migrate\destination;
+
+use Drupal\migrate\Entity\Migration;
+use Drupal\migrate\Row;
+
+/**
+ * @PluginId("entity_form_display")
+ */
+class EntityDisplay extends DestinationBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function import(Row $row) {
+    $options = $row->getDestinationProperty('options') ?: array();
+    entity_get_display($row->getDestinationProperty('entity_type'), $row->getDestinationProperty('bundle'), $row->getDestinationProperty('view_mode'))
+      ->setComponent($row->getDestinationProperty('field_name'), $options)
+      ->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIdsSchema() {
+    // TODO: Implement getIdsSchema() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields(Migration $migration = NULL) {
+    // TODO: Implement fields() method.
+  }
+
+}
