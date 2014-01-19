5feca0dc68bd334089978cde30c32daa44c85f0f
Karoly Negyesi <chx1975@gmail.com>
2014-01-19 00:12:57 -0800

    Remove hack, apply #2175017

--- core/lib/Drupal/Core/Field/FieldDefinition.php
+++ core/lib/Drupal/Core/Field/FieldDefinition.php
@@ -39,7 +39,15 @@ class FieldDefinition extends ListDefinition implements FieldDefinitionInterface
    *   A new field definition object.
    */
   public static function create($type) {
-    return new static(array(), DataDefinition::create('field_item:' . $type));
+    // Create a definition for the items, and initialize it with the default
+    // settings for the field type.
+    // @todo Cleanup in https://drupal.org/node/2116341.
+    $item_definition = DataDefinition::create('field_item:' . $type);
+    $field_type_manager = \Drupal::service('plugin.manager.field.field_type');
+    $default_settings = $field_type_manager->getDefaultSettings($type) + $field_type_manager->getDefaultInstanceSettings($type);
+    $item_definition->setSettings($default_settings);
+
+    return new static(array(), $item_definition);
   }
 
   /**
@@ -285,7 +293,6 @@ class FieldDefinition extends ListDefinition implements FieldDefinitionInterface
       // Get the schema from the field item class.
       $definition = \Drupal::service('plugin.manager.field.field_type')->getDefinition($this->getType());
       $class = $definition['class'];
-      $this->setSettings($definition['settings']);
       $schema = $class::schema($this);
       // Fill in default values for optional entries.
       $schema += array('indexes' => array(), 'foreign keys' => array());
