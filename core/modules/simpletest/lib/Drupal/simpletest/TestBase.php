b1faafba9532afc2f02a95091667f57f4227a7e4
Sascha Grossenbacher <saschagros@gmail.com>
2014-01-28 08:43:42 +0100

    bad testbot, bad.

--- core/modules/simpletest/lib/Drupal/simpletest/TestBase.php
+++ core/modules/simpletest/lib/Drupal/simpletest/TestBase.php
@@ -921,7 +921,7 @@ abstract class TestBase {
    * @see TestBase::beforePrepareEnvironment()
    */
   private function prepareEnvironment() {
-    global $user;
+    global $user, $conf;
 
     // Allow (base) test classes to backup global state information.
     $this->beforePrepareEnvironment();
@@ -940,6 +940,7 @@ abstract class TestBase {
 
     // Backup current in-memory configuration.
     $this->originalSettings = settings()->getAll();
+    $this->originalConf = $conf;
 
     // Backup statics and globals.
     $this->originalContainer = clone \Drupal::getContainer();
@@ -1035,6 +1036,9 @@ abstract class TestBase {
     // Change the database prefix.
     $this->changeDatabasePrefix();
 
+    // Reset all variables to perform tests in a clean environment.
+    $conf = array();
+
     drupal_set_time_limit($this->timeLimit);
   }
 
@@ -1112,7 +1116,7 @@ abstract class TestBase {
    * @see TestBase::prepareEnvironment()
    */
   private function restoreEnvironment() {
-    global $user;
+    global $user, $conf;
 
     // Reset all static variables.
     // Unsetting static variables will potentially invoke destruct methods,
@@ -1173,6 +1177,7 @@ abstract class TestBase {
     drupal_static_reset();
 
     // Restore original in-memory configuration.
+    $conf = $this->originalConf;
     new Settings($this->originalSettings);
 
     // Restore original statics and globals.
