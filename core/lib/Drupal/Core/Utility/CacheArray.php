17826524c42158fd6b9360b7c037afea20669cc5
Sascha Grossenbacher <saschagros@gmail.com>
2014-02-01 13:57:01 +0100

    applied patch

--- core/lib/Drupal/Core/Utility/CacheArray.php
+++ core/lib/Drupal/Core/Utility/CacheArray.php
@@ -201,13 +201,13 @@ abstract class CacheArray implements \ArrayAccess {
     // Lock cache writes to help avoid stampedes.
     // To implement locking for cache misses, override __construct().
     $lock_name = $this->cid . ':' . $this->bin;
-    if (!$lock || lock()->acquire($lock_name)) {
+    if (!$lock || \Drupal::lock()->acquire($lock_name)) {
       if ($cached = cache($this->bin)->get($this->cid)) {
         $data = $cached->data + $data;
       }
       cache($this->bin)->set($this->cid, $data, Cache::PERMANENT, $this->tags);
       if ($lock) {
-        lock()->release($lock_name);
+        \Drupal::lock()->release($lock_name);
       }
     }
   }
