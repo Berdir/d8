5d7428f928d7cab12fa4cb9bad3b71d17e11668d
Karoly Negyesi <chx1975@gmail.com>
2014-02-21 17:53:51 +1300

    removed unnecessary dump

--- core/modules/migrate_drupal/lib/Drupal/migrate_drupal/Tests/d6/MigrateUploadFieldTest.php
+++ core/modules/migrate_drupal/lib/Drupal/migrate_drupal/Tests/d6/MigrateUploadFieldTest.php
@@ -33,10 +33,6 @@ class MigrateUploadFieldTest extends MigrateDrupalTestBase {
    */
   public function testUpload() {
     $migration = entity_load('migration', 'd6_upload_field');
-    $dumps = array(
-      drupal_get_path('module', 'migrate_drupal') . '/lib/Drupal/migrate_drupal/Tests/Dump/Drupal6UploadField.php',
-    );
-    $this->prepare($migration, $dumps);
     $executable = new MigrateExecutable($migration, $this);
     $executable->import();
 
