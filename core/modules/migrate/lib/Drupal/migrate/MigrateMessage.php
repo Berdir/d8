e7ae266c8fa71351e0e85fffca11dea5f88f19d9
Karoly Negyesi <chx1975@gmail.com>
2014-02-25 17:56:53 -0800

    log migrate messages into watchdog.

--- core/modules/migrate/lib/Drupal/migrate/MigrateMessage.php
+++ core/modules/migrate/lib/Drupal/migrate/MigrateMessage.php
@@ -13,10 +13,20 @@ namespace Drupal\migrate;
 class MigrateMessage implements MigrateMessageInterface {
 
   /**
+   * The map between migrate status and watchdog severity.
+   *
+   * @var array
+   */
+  protected $map = array(
+    'status' => WATCHDOG_INFO,
+    'error' => WATCHDOG_ERROR,
+  );
+
+  /**
    * {@inheritdoc}
    */
   public function display($message, $type = 'status') {
-    drupal_set_message($message, $type);
+    watchdog('migrate', $message, array(), isset($this->map[$type]) ? $this->map[$type] : WATCHDOG_NOTICE);
   }
 
 }
