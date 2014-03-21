f51c1cef4bad19c8d612d2e4618a7074821c92a3
Sascha Grossenbacher <saschagros@gmail.com>
2014-03-21 10:24:13 +0100

    xhprof index.php

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
  * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
 
+$xhprof_path = '/var/www/html';
+include_once $xhprof_path . '/xhprof_lib/utils/xhprof_lib.php';
+include_once $xhprof_path . '/xhprof_lib/utils/xhprof_runs.php';
+xhprof_enable(XHPROF_FLAGS_NO_BUILTINS + XHPROF_FLAGS_MEMORY);
+
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -20,3 +25,9 @@ $response = $kernel->handle($request);
 $response->send();
 
 $kernel->terminate($request, $response);
+
+$xhprof_data = xhprof_disable();
+$xhprof_runs = new XHProfRuns_Default();
+$namespace = 'd8';
+$id = $xhprof_runs->save_run($xhprof_data, $namespace);
+print "<a href='http://localhost/xhprof_html/?run=$id&sort=excl_wt&source=$namespace'>XHPROF</a>";
