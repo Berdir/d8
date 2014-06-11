e6c6c556a66f9b4ab58bd259d105a241c861aaca
Sascha Grossenbacher <saschagros@gmail.com>
2014-06-11 09:19:45 +0200

    fix stuff

--- core/modules/entity_reference/src/Tests/Views/EntityReferenceRelationshipTest.php
+++ core/modules/entity_reference/src/Tests/Views/EntityReferenceRelationshipTest.php
@@ -7,8 +7,13 @@
 
 namespace Drupal\entity_reference\Tests\Views;
 
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldInstanceConfig;
 use Drupal\views\Tests\ViewTestBase;
 use Drupal\views\Tests\ViewTestData;
+use Drupal\views\Views;
 
 /**
  * Defines a test for the entity_reference views relationship.
@@ -49,50 +54,45 @@ class EntityReferenceRelationshipTest extends ViewTestBase {
   protected function setUp() {
     parent::setUp();
 
-    ViewTestData::importTestViews(get_class($this), array('entity_reference_test_views'));
+    ViewTestData::createTestViews(get_class($this), array('entity_reference_test_views'));
 
-    $field = array(
-      'translatable' => FALSE,
-      'entity_types' => array(),
+    $field = FieldConfig::create(array(
       'settings' => array(
         'target_type' => 'entity_test',
       ),
-      'field_name' => 'field_test',
+      'entity_type' => 'entity_test',
+      'name' => 'field_test',
       'type' => 'entity_reference',
-      'cardinality' => FIELD_CARDINALITY_UNLIMITED,
-    );
+      'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
+    ));
+    $field->save();
 
-    field_create_field($field);
-
-    $instance = array(
+    $instance = FieldInstanceConfig::create(array(
       'entity_type' => 'entity_test',
       'field_name' => 'field_test',
       'bundle' => 'entity_test',
-      'widget' => array(
-        'type' => 'options_select',
-      ),
       'settings' => array(
         'handler' => 'default',
         'handler_settings' => array(),
       ),
-    );
-    field_create_instance($instance);
+    ));
+    $instance->save();
 
     // Create some test entities which link each other.
-    $storage_controller= $this->container->get('plugin.manager.entity')->getStorageController('entity_test');
-    $entity_0 = $storage_controller->create(array());
+    $entity_storage= \Drupal::entityManager()->getStorage('entity_test');
+    $entity_0 = $entity_storage->create(array());
     // @todo Add a value in order to avoid: "Column 'field_test_target_id'
     // cannot be null".
-    $entity_0->field_test->target_id = 1000;
+    //$entity_0->field_test->target_id = 1000;
     $entity_0->save();
     $this->entities[$entity_0->id()] = $entity_0;
 
-    $entity = $storage_controller->create(array());
+    $entity = $entity_storage->create(array());
     $entity->field_test->target_id = $entity_0->id();
     $entity->save();
     $this->entities[$entity->id()] = $entity;
 
-    $entity = $storage_controller->create(array('field_test' => $entity->id()));
+    $entity = $entity_storage->create(array('field_test' => $entity->id()));
     $entity->field_test->target_id = $entity_0->id();
     $entity->save();
     $this->entities[$entity->id()] = $entity;
@@ -103,7 +103,7 @@ class EntityReferenceRelationshipTest extends ViewTestBase {
    */
   public function testRelationship() {
     // Check just the generated views data.
-    $views_data_field_test = $this->container->get('views.views_data')->get('field_data_field_test');
+    $views_data_field_test = $this->container->get('views.views_data')->get('entity_test__field_test');
     $this->assertEqual($views_data_field_test['field_test']['relationship']['id'], 'standard');
     $this->assertEqual($views_data_field_test['field_test']['relationship']['base'], 'entity_test');
     $this->assertEqual($views_data_field_test['field_test']['relationship']['base field'], 'id');
@@ -111,24 +111,24 @@ class EntityReferenceRelationshipTest extends ViewTestBase {
 
     // Check the backwards reference.
     $views_data_entity_test = $this->container->get('views.views_data')->get('entity_test');
-    $this->assertEqual($views_data_entity_test['reverse_field_test']['relationship']['id'], 'entity_reverse');
-    $this->assertEqual($views_data_entity_test['reverse_field_test']['relationship']['base'], 'entity_test');
-    $this->assertEqual($views_data_entity_test['reverse_field_test']['relationship']['base field'], 'id');
-    $this->assertEqual($views_data_entity_test['reverse_field_test']['relationship']['field table'], 'field_data_field_test');
-    $this->assertEqual($views_data_entity_test['reverse_field_test']['relationship']['field field'], 'field_test_target_id');
+    $this->assertEqual($views_data_entity_test['reverse_entity_test__field_test']['relationship']['id'], 'entity_reverse');
+    $this->assertEqual($views_data_entity_test['reverse_entity_test__field_test']['relationship']['base'], 'entity_test');
+    $this->assertEqual($views_data_entity_test['reverse_entity_test__field_test']['relationship']['base field'], 'id');
+    $this->assertEqual($views_data_entity_test['reverse_entity_test__field_test']['relationship']['field table'], 'entity_test__field_test');
+    $this->assertEqual($views_data_entity_test['reverse_entity_test__field_test']['relationship']['field field'], 'field_test_target_id');
 
 
     // Check an actually test view.
-    $view = views_get_view('test_entity_reference_view');
+    $view = Views::getView('test_entity_reference_view');
     $this->executeView($view, 'default');
     $view->initStyle();
 
     foreach (array_keys($view->result) as $index) {
       // Just check that the actual ID of the entity is the expected one.
-      $this->assertEqual($view->style_plugin->get_field($index, 'id'), $this->entities[$index + 1]->id());
-      // Test the foward relationsnip.
+      $this->assertEqual($view->style_plugin->getField($index, 'id'), $this->entities[$index + 1]->id());
+      // Test the forward relationship.
       // The second and third entity refer to the first one.
-      $this->assertEqual($view->style_plugin->get_field($index, 'id_1'), $index == 0 ? 0 : 1);
+      $this->assertEqual($view->style_plugin->getField($index, 'id_1'), $index == 0 ? 0 : 1);
     }
 
     $view->destroy();
@@ -136,11 +136,9 @@ class EntityReferenceRelationshipTest extends ViewTestBase {
     $view->initStyle();
 
     foreach (array_keys($view->result) as $index) {
-      debug($view->style_plugin->get_field($index, 'id'));
-      debug($view->style_plugin->get_field($index, 'id_1'));
-//      $this->assertEqual($view->style_plugin->get_field($index, 'id'), $this->entities[$index + 1]->id());
-//       The second and third entity refer to the first one.
-//      $this->assertEqual($view->style_plugin->get_field($index, 'id_1'), $index == 0 ? 0 : 1);
+      $this->assertEqual($view->style_plugin->getField($index, 'id'), $this->entities[$index + 1]->id());
+      // The second and third entity refer to the first one.
+       $this->assertEqual($view->style_plugin->getField($index, 'id_1'), $index == 0 ? 0 : 1);
     }
   }
 
