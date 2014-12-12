56612396689183ddb53b37d54f1db092ae8aa0e4
Sascha Grossenbacher <saschagros@gmail.com>
2014-12-12 14:15:47 +0100

    unit test

--- core/modules/system/src/Tests/Cache/ChainedFastBackendUnitTest.php
+++ core/modules/system/src/Tests/Cache/ChainedFastBackendUnitTest.php
@@ -21,33 +21,13 @@ class ChainedFastBackendUnitTest extends GenericCacheBackendUnitTestBase {
   /**
    * Creates a new instance of ChainedFastBackend.
    *
-   * @return
+   * @return \Drupal\Core\Cache\ChainedFastBackend
    *   A new ChainedFastBackend object.
    */
   protected function createCacheBackend($bin) {
     $consistent_backend = new DatabaseBackend($this->container->get('database'), $bin);
     $fast_backend = new PhpBackend($bin);
     return new ChainedFastBackend($consistent_backend, $fast_backend, $bin);
-  }
-
-  /**
-   * Tests the expiration when the fast backend is invalidated.
-   */
-  public function testExpirationonWriteThrough() {
-    $backend = $this->getCacheBackend();
-
-    $backend->set('expired_data', 'foobar', REQUEST_TIME - 3);
-
-    // Write a different cache entry to invalidate the fast backend and force
-    // a write through from the consistent to the fast backend.
-    $backend->set('other', 'data');
-
-    $this->assertFalse($backend->get('expired_data'), 'Invalid item not returned.');
-    $cached = $backend->get('expired_data', TRUE);
-    $this->assert(is_object($cached), 'Backend returned an object for cache id expired_data.');
-    $this->assertFalse($cached->valid, 'Item is marked as invalid.');
-    $this->assertTrue($cached->created >= REQUEST_TIME && $cached->created <= round(microtime(TRUE), 3), 'Created time is correct.');
-    $this->assertEqual($cached->expire, REQUEST_TIME - 3, 'Expire time is correct.');
   }
 
 }
