98ceb950c37a0925727d6236f36629d8a89471c1
Sascha Grossenbacher <saschagros@gmail.com>
2015-03-03 18:46:26 +0100

    add comment

--- core/modules/basic_auth/src/PageCache/DisallowBasicAuthRequests.php
+++ core/modules/basic_auth/src/PageCache/DisallowBasicAuthRequests.php
@@ -13,7 +13,9 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Cache policy for pages served from basic auth.
  *
- * This policy disallows caching of requests that use basic_auth.
+ * This policy disallows caching of requests that use basic_auth for security
+ * reasons. Otherwise responses for authenticated requests can get into the
+ * page cache and could be delivered to unprivileged users.
  */
 class DisallowBasicAuthRequests implements RequestPolicyInterface {
 
