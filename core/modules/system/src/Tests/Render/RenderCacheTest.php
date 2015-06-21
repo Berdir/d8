26f0c2f1102331812e2899406a6f0877e564221b
Sascha Grossenbacher <saschagros@gmail.com>
2015-06-21 11:00:35 +0200

    convert to kernel test, use correct context

--- core/modules/system/src/Tests/Render/RenderCacheTest.php
+++ core/modules/system/src/Tests/Render/RenderCacheTest.php
@@ -7,29 +7,47 @@
 
 namespace Drupal\system\Tests\Render;
 
-use Drupal\simpletest\WebTestBase;
-use Drupal\user\Entity\User;
+use Drupal\simpletest\KernelTestBase;
+use Drupal\simpletest\UserCreationTrait;
 
 /**
  * Tests the caching of render items via functional tests.
  *
  * @group Render
  */
-class RenderCacheTest extends WebTestBase {
+class RenderCacheTest extends KernelTestBase {
+
+  use UserCreationTrait;
 
   /**
-   * Tests the caching of render items.
+   * Modules to enable.
+   *
+   * @var array
    */
-  public function testDrupalRenderCache() {
-    // Force a request via GET.
-    $request_method = \Drupal::request()->getMethod();
-    \Drupal::request()->setMethod('GET');
+  public static $modules = ['user', 'system'];
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installEntitySchema('user');
+    $this->installConfig(['user']);
+    $this->installSchema('system', ['sequences']);
+  }
+
+  /**
+   * Tests that user 1 has a different permission context with the same roles.
+   */
+  public function testUser1PermissionContext() {
     // Test that user 1 does not share the cache with other users who have the
     // same roles, even when using a role-based cache context.
-    $user1 = User::load(1);
-    $first_authenticated_user = $this->drupalCreateUser();
-    $second_authenticated_user = $this->drupalCreateUser();
+    $user1 = $this->createUser();
+    $this->assertEqual($user1->id(), 1);
+    $first_authenticated_user = $this->createUser();
+    $second_authenticated_user = $this->createUser();
+    $admin_user = $this->createUser([], NULL, TRUE);
+
     $this->assertEqual($user1->getRoles(), $first_authenticated_user->getRoles(), 'User 1 has the same roles as an authenticated user.');
     // Impersonate user 1 and render content that only user 1 should have
     // permission to see.
@@ -37,13 +55,14 @@ class RenderCacheTest extends WebTestBase {
     $test_element = array(
       '#cache' => array(
         'keys' => array('test'),
-        'contexts' => array('user.roles'),
+        'contexts' => array('user.permissions'),
       ),
     );
     $element = $test_element;
     $element['#markup'] = 'content for user 1';
     $output = \Drupal::service('renderer')->render($element);
     $this->assertEqual($output, 'content for user 1');
+
     // Verify the cache is working by rendering the same element but with
     // different markup passed in; the result should be the same.
     $element = $test_element;
@@ -51,6 +70,7 @@ class RenderCacheTest extends WebTestBase {
     $output = \Drupal::service('renderer')->render($element);
     $this->assertEqual($output, 'content for user 1');
     \Drupal::service('account_switcher')->switchBack();
+
     // Verify that the first authenticated user does not see the same content
     // as user 1.
     \Drupal::service('account_switcher')->switchTo($first_authenticated_user);
@@ -59,6 +79,7 @@ class RenderCacheTest extends WebTestBase {
     $output = \Drupal::service('renderer')->render($element);
     $this->assertEqual($output, 'content for authenticated users');
     \Drupal::service('account_switcher')->switchBack();
+
     // Verify that the second authenticated user shares the cache with the
     // first authenticated user.
     \Drupal::service('account_switcher')->switchTo($second_authenticated_user);
@@ -68,8 +89,14 @@ class RenderCacheTest extends WebTestBase {
     $this->assertEqual($output, 'content for authenticated users');
     \Drupal::service('account_switcher')->switchBack();
 
-    // Restore the previous request method.
-    \Drupal::request()->setMethod($request_method);
+    // Verify that the admin user (who has an admin role without explicit
+    // permissions) does not share the same cache.
+    \Drupal::service('account_switcher')->switchTo($admin_user);
+    $element = $test_element;
+    $element['#markup'] = 'content for admin user';
+    $output = \Drupal::service('renderer')->render($element);
+    $this->assertEqual($output, 'content for admin user');
+    \Drupal::service('account_switcher')->switchBack();
   }
 
 }
