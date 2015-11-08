164764f92075dbf8111bffca05631250e70352a9
Sascha Grossenbacher <saschagros@gmail.com>
2015-11-08 14:38:00 +0100

    run itself

--- core/modules/simpletest/src/Tests/SimpleTestWithLanguageTest.php
+++ core/modules/simpletest/src/Tests/SimpleTestWithLanguageTest.php
@@ -27,19 +27,25 @@ class SimpleTestWithLanguageTest extends WebTestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->drupalLogin($this->drupalCreateUser(array('administer unit tests')));
   }
 
   /**
    * Ensures the tests run well with language module enabled.
    */
   protected function testSimpleTestWithLanguageModule() {
-    $edit['tests[Drupal\language\Tests\LanguageListTest]'] = TRUE;
-    $this->drupalPostForm('admin/config/development/testing', $edit, t('Run tests'));
-    $this->assertRaw('0 exceptions');
-    $this->assertRaw('0 fails');
-    if (isset($this->xpath('//a[normalize-space(text())=:label]', array(':label' => 'the error page'))[0])) {
-      $this->clickLink('the error page');
+    if ($this->isInChildSite()) {
+      $this->pass('Child site test executed');
+    }
+    else {
+      $this->drupalLogin($this->drupalCreateUser(array('administer unit tests')));
+      $edit['tests[Drupal\simpletest\Tests\SimpleTestWithLanguageTest]'] = TRUE;
+      $this->drupalPostForm('admin/config/development/testing', $edit, t('Run tests'));
+      $this->assertRaw('0 exceptions');
+      $this->assertRaw('0 fails');
+      $this->assertText('Child site test executed');
+      if (isset($this->xpath('//a[normalize-space(text())=:label]', array(':label' => 'the error page'))[0])) {
+        $this->clickLink('the error page');
+      }
     }
   }
 
